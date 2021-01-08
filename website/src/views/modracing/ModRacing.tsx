a8913279b2d4481903b9a5980b79e7e3e6d5cd64
jovyn <jovyn.tls@gmail.com>
2021-01-09 04:49:20 +0800

    add API for start and end points

--- website/src/views/modracing/ModRacing.tsx
+++ website/src/views/modracing/ModRacing.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import ModList from "./ModList"
+import axios from "axios";
+import { Link } from "react-router-dom"
 
 import classnames from 'classnames';
 import styles from './ModRacing.scss'
@@ -29,9 +31,22 @@ export default class ModRacing extends React.PureComponent<Props, State> {
     };
   }
 
+  componentDidMount() {
+    axios.get('http://simpson-mods-api.herokuapp.com/path_random')
+      .then((res) => {
+        this.setState({destination: res.data.mod2});
+        this.setState({start_point: res.data.mod1});
+        console.log(res.data)
+        return res.data
+      })
+      .then((res) => {
+        console.log(this.state.start_point)
+      })
+      .catch((err) => console.log(err));
+  }
+
   start_game = () => {
     this.setState({gameStarted: true});
-    this.setState({destination: this.set_destination()})
     this.setState({gameWon: false})
   }
   
@@ -40,28 +55,26 @@ export default class ModRacing extends React.PureComponent<Props, State> {
     this.setState({gameWon: true})
   }
 
-  set_destination = () => {
-    return "CS1231S";
-  }
-
   isGameActive = () => {
     return this.state.gameStarted && !this.state.gameWon;
   }
 
 
   render() {
-    // const { value, placeholder, isLoading } = this.props;
     return (
       <div className={styles.myDiv}>
         {this.state.gameStarted || this.state.gameWon
           ? <ModList start_point={this.state.start_point} destination={this.state.destination} end_game={this.end_game} isGameActive={this.isGameActive} restart_game={this.start_game} /> 
-          : <button 
-                type="button"
-                className={classnames('btn btn-outline-primary btn-svg')}
-                onClick={this.start_game} ref="/modules/MA1100/"
+          : <Link to={"/modules/" + this.state.start_point + "/"}>
+            <button 
+              type="button"
+              className={classnames('btn btn-outline-primary btn-svg')}
+              onClick={this.start_game}
             >
-            Start Game
-            </button>}
+              Start Game
+            </button>
+          </Link>
+        }
       </div>
     );
   }
